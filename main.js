5a0a8d9292d2b346bee4bb1820d1ea5a1cd7b946
Bradley Newman <bnewman227@gmail.com>
2020-07-09 11:33:15 +0930

    Native theme switching, Create light theme

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-const { app, BrowserWindow, BrowserView } = require('electron')
+const { app, BrowserWindow, BrowserView, nativeTheme } = require('electron')
 const path = require('path')
 const fs = require("fs")
 
 
 var lightTheme = fs.readFileSync('theme-light.css')
 var darkTheme = fs.readFileSync('theme-dark.css')
-var titlebarTheme = fs.readFileSync('title.css')
+var titlebarThemeDark = fs.readFileSync('title-dark.css')
+var titlebarThemeLight = fs.readFileSync('title-light.css')
 var domain = fs.readFileSync('domain.txt')
-var titlebar = fs.readFileSync('title.html')
+var titlebarDark = fs.readFileSync('title-dark.html')
+var titlebarLight = fs.readFileSync('title-light.html')
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 1280,
+    width: 1500,
     height: 800,
     frame: false,
     backgroundColor: '#FFF',
@@ -33,12 +35,16 @@ function createWindow() {
 
   contentView.webContents.on('did-finish-load', function() {
     contentView.webContents.setZoomFactor(1.2)
-    contentView.webContents.insertCSS(darkTheme.toString())
+    if (nativeTheme.shouldUseDarkColors) {
+      contentView.webContents.insertCSS(darkTheme.toString())
+    } else {
+      contentView.webContents.insertCSS(lightTheme.toString())
+    }
   })
 
   mainWindow.addBrowserView(contentView)
 
-  contentView.setBounds({ x: 0, y: 30, width: 1280, height: 770 })
+  contentView.setBounds({ x: 0, y: 30, width: 1500, height: 770 })
 
   contentView.setAutoResize({
     width: true, 
@@ -46,12 +52,19 @@ function createWindow() {
   })
 
   contentView.webContents.loadURL(domain.toString())
-
-  mainWindow.webContents.loadURL("data:text/html," + titlebar.toString())
+  if (nativeTheme.shouldUseDarkColors) {
+    mainWindow.webContents.loadURL("data:text/html," + titlebarDark.toString())
+  } else {
+    mainWindow.webContents.loadURL("data:text/html," + titlebarLight.toString())
+  }
 
   mainWindow.webContents.on('did-finish-load', function () {
     mainWindow.setTitle("Spooky Chat")
-    mainWindow.webContents.insertCSS(titlebarTheme.toString())
+    if (nativeTheme.shouldUseDarkColors) {
+      mainWindow.webContents.insertCSS(titlebarThemeDark.toString())
+    } else {
+      mainWindow.webContents.insertCSS(titlebarThemeLight.toString())
+    }
   })
 
   // mainWindow.webContents.openDevTools()
